02665863d0d132bfa5843a06fbe26eef155d8db6
RenzoMag <renzomagnoni203@gmail.com>
2022-11-21 00:16:22 -0300

    Tarjetas añadidas

--- Front/main.js
+++ Front/main.js
@@ -1,9 +1,22 @@
 const cartel = document.querySelectorAll('.carteles');
+const car = document.querySelectorAll('.cartelact');
+console.log(car)
+console.log(cartel)
+
+
 
 cartel.forEach((carteles) => {
-
     carteles.addEventListener("click", () =>{
         carteles.classList.toggle("activado");
     });
-
 });
+
+cartel.forEach((carteles) => {
+    carteles.addEventListener("click", () =>{
+            car.forEach((cars) => {
+                cars.classList.toggle("oculto");
+            });
+    });
+});
+
+
