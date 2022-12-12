d0b8aaefc23e5e94e1b9d1a806d69e44278a40e6
RenzoMag <renzomagnoni203@gmail.com>
2022-12-11 21:53:46 -0300

    correccion de errores

--- Front/main.js
+++ Front/main.js
@@ -1,22 +1,24 @@
-const cartel = document.querySelectorAll('.carteles');
-const car = document.querySelectorAll('.cartelact');
-console.log(car)
-console.log(cartel)
+    const cartel = document.getElementById('mic');
+    const cont = document.getElementById('micc');
 
-
-
-cartel.forEach((carteles) => {
-    carteles.addEventListener("click", () =>{
-        carteles.classList.toggle("activado");
-    });
+cartel.addEventListener("click", () =>{
+    cartel.classList.toggle("activado");
+    cont.classList.toggle("oculto");
 });
 
-cartel.forEach((carteles) => {
-    carteles.addEventListener("click", () =>{
-            car.forEach((cars) => {
-                cars.classList.toggle("oculto");
-            });
-    });
+    const cartel1 = document.getElementById('pyc');
+    const cont1 = document.getElementById('pycc');
+
+cartel1.addEventListener("click", () =>{
+    cartel1.classList.toggle("activado");
+    cont1.classList.toggle("oculto");
+    cartel1.classList.toggle("pyccc");
 });
 
+    const cartel2 = document.getElementById('arc');
+    const cont2 = document.getElementById('arcc');
 
+cartel2.addEventListener("click", () =>{
+    cartel2.classList.toggle("activado");
+    cont2.classList.toggle("oculto");
+});
