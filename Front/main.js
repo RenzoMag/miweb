9552d68d67d108472d24eceafcdae73aab83ea09
RenzoMag <renzomagnoni203@gmail.com>
2022-12-26 17:35:01 -0300

    .

--- Front/main.js
+++ Front/main.js
@@ -1,9 +1,20 @@
+    const linea = document.getElementById('linea');
+
     const cartel = document.getElementById('mic');
     const cont = document.getElementById('micc');
 
 cartel.addEventListener("click", () =>{
     cartel.classList.toggle("activado");
     cont.classList.toggle("oculto");
+
+    cartel1.classList.remove("activado");
+    cont1.classList.remove("oculto");
+    cartel2.classList.remove("activado");
+    cont2.classList.remove("oculto");
+    cartel3.classList.remove("activado");
+    cont3.classList.remove("oculto");
+
+    linea.classList.toggle("lineapri")
 });
 
     const cartel1 = document.getElementById('pyc');
@@ -12,6 +23,15 @@ cartel.addEventListener("click", () =>{
 cartel1.addEventListener("click", () =>{
     cartel1.classList.toggle("activado");
     cont1.classList.toggle("oculto");
+
+    cartel.classList.remove("activado");
+    cont.classList.remove("oculto");
+    cartel2.classList.remove("activado");
+    cont2.classList.remove("oculto");
+    cartel3.classList.remove("activado");
+    cont3.classList.remove("oculto");
+
+    linea.classList.toggle("lineasec")
 });
 
     const cartel2 = document.getElementById('arc');
@@ -20,14 +40,32 @@ cartel1.addEventListener("click", () =>{
 cartel2.addEventListener("click", () =>{
     cartel2.classList.toggle("activado");
     cont2.classList.toggle("oculto");
+
+    cartel1.classList.remove("activado");
+    cont1.classList.remove("oculto");
+    cartel.classList.remove("activado");
+    cont.classList.remove("oculto");
+    cartel3.classList.remove("activado");
+    cont3.classList.remove("oculto");
+
+    linea.classList.toggle("lineapri")
 });
 
 const cartel3 = document.getElementById('qbas');
 const cont3 = document.getElementById('qbass');
 
 cartel3.addEventListener("click", () =>{
-cartel3.classList.toggle("activado");
-cont3.classList.toggle("oculto");
+    cartel3.classList.toggle("activado");
+    cont3.classList.toggle("oculto");
+
+    cartel1.classList.remove("activado");
+    cont1.classList.remove("oculto");
+    cartel2.classList.remove("activado");
+    cont2.classList.remove("oculto");
+    cartel.classList.remove("activado");
+    cont.classList.remove("oculto");
+
+    linea.classList.toggle("lineasec")
 });
 
 const proyecto = document.getElementsByClassName('adms');
@@ -40,3 +78,4 @@ proyecto[0].onmouseover= function(){
 proyecto[0].onmouseout= function(){
     proyecto2[0].classList.remove("admscarton")
 };
+
